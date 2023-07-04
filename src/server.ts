b3882a4407b4724826ff336c295d39a8b8b86dbf
josevitorsoares <josevitorgoncalvessoares@gmail.com>
2023-07-04 06:36:09 -0300

    FEAT: Adding UI swagger to route documentation

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
+import swaggerFile from "./swagger.json";
+
 import { router } from "./routes";
 import { client } from "../database/db";
-
 client;
 
 const app = express();
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(express.json());
 
