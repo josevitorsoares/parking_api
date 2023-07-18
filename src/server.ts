3b74be17962748db1263b7cc75a4a8f1f943b7bb
josevitorsoares <josevitorgoncalvessoares@gmail.com>
2023-07-18 06:35:25 -0300

    FEAT: Implementing error middleware

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import swaggerUi from "swagger-ui-express";
 import swaggerFile from "./swagger.json";
 
 import { router } from "./routes";
 import { client } from "../database/db";
+import { AppError } from "./http/middlwares/AppError";
 client;
 
 const app = express();
@@ -14,6 +15,21 @@ app.use(express.json());
 
 app.use(router);
 
+app.use((error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+        return response.status(error.statusCode).json({
+            message: error.message
+        });
+    }
+
+    return response.status(500).json({
+        status: "Error",
+        message: `Internal server error - ${error.message}`
+    });
+
+    next();
+});
+
 app.listen(3333, ()=> {
     console.log("🚀 Server runing!");
-})
+});
