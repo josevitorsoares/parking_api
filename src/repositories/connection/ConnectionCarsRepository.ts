fc5995f92cf1b32d7289930481506977a3ec5bfa
josevitorsoares <josevitorgoncalvessoares@gmail.com>
2023-07-21 06:48:00 -0300

    FEAT: Implementing repositories

---
+++ src/repositories/connection/ConnectionCarsRepository.ts
@@ -0,0 +1,43 @@
+import { client } from "../../../database/db";
+import { ICars } from "../CarsRepository";
+import { Cars } from "../../models/Cars";
+import { AppError } from "../../http/middlwares/AppError";
+
+export class CarsRepository implements ICars{
+    async create(owner: string, license_plate: string, telephone: string): Promise<void>{
+        const car = new Cars();
+
+        const carLicensePlate = await this.verifyLicensePlate(license_plate);
+        
+        if (carLicensePlate) {
+            throw new AppError("There is already a car with this license plate!", 422);
+        }
+
+        car.owner = owner;
+        car.license_plate = license_plate;
+        car.telephone = telephone;
+
+        const query = "INSERT INTO cars (id, owner, license_plate, telephone) VALUES ($1, $2, $3, $4)";
+        const values = [car.id, car.owner, car.license_plate, car.telephone];
+
+        try {
+            await client.query(query, values).then(() => console.log("Car was created")).catch((error) => console.error(error));
+        } catch (error) {
+            console.error("Error creating Car: ", error);
+        }
+    }
+    
+    async findByID(id: string) {
+        const query = "SELECT id FROM cars WHERE id = $1";
+
+        const car = await client.query(query, [id]);
+        return car.rows[0];
+    }
+
+    async verifyLicensePlate(license_plate: string){
+        const query = "SELECT license_plate FROM cars WHERE license_plate = $1";
+
+        const car = await client.query(query, [license_plate]);
+        return car.rows[0];
+    }
+}
---
+++ src/repositories/connection/ConnectionVacanciesRepository.ts
@@ -0,0 +1,66 @@
+import { client } from "../../../database/db";
+import { IVacancies } from "../VacanciesRepository";
+import { Vacancies } from "../../models/Vacancies";
+import { AppError } from "../../http/middlwares/AppError";
+
+export class VacanciesRepository implements IVacancies{
+    async create(vacancy_number: string): Promise<void> {
+        const vacancies = new Vacancies();
+
+        const vacancyNumber = await this.verifyVacancyNumber(vacancy_number);
+
+        if (vacancyNumber) {
+            throw new AppError("Vacancy number already exists", 422);
+        }
+
+        vacancies.vacancy_number = vacancy_number;
+
+        const query = "INSERT INTO vacancies (id, vacancy_number, available) VALUES ($1, $2, $3)";
+        const values = [vacancies.id, vacancies.vacancy_number, vacancies.available];
+
+        try {
+            await client.query(query, values).then(() => console.log("Vacancy was created!")).catch((error) => console.error(error));
+        } catch (error) {
+            console.error("Error creating Vacancy: ", error);
+        }
+    }
+
+    async updateAvailableVacancy(vacancy_id: string, status: boolean) {
+        const query = "UPDATE vacancies SET available = $1 WHERE id = $2";
+        const values = [status, vacancy_id];
+
+        try {
+            await client.query(query, values).then(() => console.log("Vacancy was updated")).catch((error) => console.error(error));
+        } catch (error) {
+            console.error("Error updating Vacancy: ", error);
+        }
+    }
+
+    async findByID(id: string) {
+        const query = "SELECT id FROM vacancies WHERE id = $1";
+
+        const vacancy = await client.query(query, [id]);
+        return vacancy.rows[0];
+    }
+
+    async verifyAvaliableVacancy(vacancy_id: string) {
+        const query = "SELECT id FROM vacancies WHERE id = $1 AND available = true";
+
+        const vacancy = await client.query(query, [vacancy_id]);
+        return vacancy.rows[0];
+    }
+
+    async verifyAllAvailableVacancies(): Promise<number> {
+        const query = "SELECT available FROM vacancies WHERE available = false";
+
+        const vacancies = await client.query(query);
+        return vacancies.rowCount;
+    }
+
+    async verifyVacancyNumber(vacancy_number: string) {
+        const query = "SELECT vacancy_number FROM vacancies WHERE vacancy_number = $1";
+
+        const vacancy = await client.query(query, [vacancy_number]);
+        return vacancy.rows[0];
+    }
+}
