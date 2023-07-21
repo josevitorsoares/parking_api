f4e4ec5a748ae2cae777b80557d6333cf79b7c76
josevitorsoares <josevitorgoncalvessoares@gmail.com>
2023-07-21 13:18:10 -0300

    FEAT: Adding repository methods

--- src/repositories/ParkingRepository.ts
+++ src/repositories/ParkingRepository.ts
@@ -6,4 +6,8 @@ export interface IParking{
     verifyCarIdParking(car_id: string): Promise<void>;
     getEntryTimeParking(parking_id: string): Promise<void>;
     updateValueAndExitTimeParking(parking_id: string, value: number, exit_time: Date): Promise<void>;
+    getCurrentDate(): Date;
+    getHour(timestamp: Date): number;
+    convertToUTC(date: Date): string;
+    compareInHours(star_date: Date, end_date: Date): number;
 }
