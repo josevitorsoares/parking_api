54f3500c67e0de1bbcfc1c7c2ccb8cd1499a9dbb
josevitorsoares <josevitorgoncalvessoares@gmail.com>
2023-07-25 06:08:57 -0300

    REFACTIR: Renaming 'unparkinService'

--- src/http/controllers/parking/unparking.ts
+++ src/http/controllers/parking/unparking.ts
@@ -2,10 +2,10 @@ import { Request, Response } from "express";
 import { makeUnparkingUseCase } from "../../../useCases/factories/makeUnparkingUseCase";
 
 export async function unparking(request: Request, response: Response): Promise<Response> {
-    const parkingService = makeUnparkingUseCase();
     const { vacancy_id } = request.body;
+    const unparkinUseCase = makeUnparkingUseCase();
 
-    await parkingService.execute(vacancy_id);
+    await unparkinUseCase.execute({ vacancy_id });
 
     return response.send();
 }
