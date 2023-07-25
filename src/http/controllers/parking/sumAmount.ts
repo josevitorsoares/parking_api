040aec892b9590fcc1954583e89a0109e6bdd68a
josevitorsoares <josevitorgoncalvessoares@gmail.com>
2023-07-25 06:16:49 -0300

    FEAT: Implementing controller for 'sumAmountOnDay'

---
+++ src/http/controllers/parking/sumAmount.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from "express";
+import { makeSumAmountOnDayUseCase } from "../../../useCases/factories/makeSumAmountOnDayUseCase";
+
+export async function sumAmount(request: Request, response: Response): Promise<Response> {
+    const sumAmountUseCase = makeSumAmountOnDayUseCase();
+
+    const result = await sumAmountUseCase.execute();
+
+    return response.json({
+        value: result
+    });
+}
