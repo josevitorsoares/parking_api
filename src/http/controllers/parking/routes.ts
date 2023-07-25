0e35647c717858b9791adea008ebcd1e828eb946
josevitorsoares <josevitorgoncalvessoares@gmail.com>
2023-07-25 06:17:22 -0300

    FEAT: Implementing routes for 'sumAmountOnDay'

--- src/http/controllers/parking/routes.ts
+++ src/http/controllers/parking/routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { create } from "./parking";
 import { unparking } from "./unparking";
+import { sumAmount } from "./sumAmount";
 
 const parkingRoutes = Router();
 
 parkingRoutes.post("/create", create);
 parkingRoutes.post("/unparking", unparking);
+parkingRoutes.get("/sum", sumAmount);
 
 export { parkingRoutes };
