ec465f9b09869973a1f948cc0a776bc289a94534
josevitorsoares <josevitorgoncalvessoares@gmail.com>
2023-07-25 20:56:16 -0300

    FEAT: Adding listAllCars method

--- src/repositories/CarsRepository.ts
+++ src/repositories/CarsRepository.ts
@@ -1,6 +1,9 @@
+import { QueryResult } from "pg";
+import { Cars } from "../models/Cars";
 
 export interface ICars{
     create(owner: string, license_plate: string, telephone: string): Promise<void>;
     findByID(id: string): Promise<void>;
-    verifyLicensePlate(license_plate: string): Promise<void>
+    verifyLicensePlate(license_plate: string): Promise<void>;
+    listAllCars(): Promise<QueryResult<Cars>[]>;
 }
