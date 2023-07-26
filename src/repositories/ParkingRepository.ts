df228618c52f387c9e682979e08d4bdc5c759920
josevitorsoares <josevitorgoncalvessoares@gmail.com>
2023-07-26 06:28:18 -0300

    REFACTIR: Adding Repository method types

--- src/repositories/ParkingRepository.ts
+++ src/repositories/ParkingRepository.ts
@@ -1,10 +1,11 @@
+import { Parking } from "../models/Parking";
 
 export interface IParking{
     create(car_id: string, vacancy_id: string): Promise<void>;
     unparking(vacancy_id: string): Promise<void>;
-    verifyVacancyIdParking(vacancy_id: string): Promise<void>;
-    verifyCarIdParking(car_id: string): Promise<void>;
-    getEntryTimeParking(parking_id: string): Promise<void>;
+    verifyVacancyIdParking(vacancy_id: string): Promise<Parking>;
+    verifyCarIdParking(car_id: string): Promise<Parking>;
+    getEntryTimeParking(parking_id: string): Promise<Date>;
     updateValueAndExitTimeParking(parking_id: string, value: number, exit_time: Date): Promise<void>;
     getCurrentDate(): Date;
     getHour(timestamp: Date): number;
--- src/repositories/VacanciesRepository.ts
+++ src/repositories/VacanciesRepository.ts
@@ -1,8 +1,10 @@
+import { Vacancies } from "../models/Vacancies";
+
 export interface IVacancies{
     create(vacancy_number: string): Promise<void>;
     updateAvailableVacancy(vacancy_id: string, status: boolean): Promise<void>;
-    findByID(id: string): Promise<void>;
-    verifyAvaliableVacancy(vacancy_id: string): Promise<void>;
+    findByID(id: string): Promise<Vacancies>;
+    verifyAvaliableVacancy(vacancy_id: string): Promise<Vacancies>;
     verifyAllAvailableVacancies(): Promise<number>;
-    verifyVacancyNumber(vacancy_number: string): Promise<void>; 
+    verifyVacancyNumber(vacancy_number: string): Promise<Vacancies>; 
 }
