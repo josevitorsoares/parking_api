c23fce384cb84d18e47d93f27e6b05ccd5115b70
josevitorsoares <josevitorgoncalvessoares@gmail.com>
2023-07-27 06:15:49 -0300

    REFACTIR: Adding Repository method types

--- src/repositories/CarsRepository.ts
+++ src/repositories/CarsRepository.ts
@@ -1,9 +1,8 @@
-import { QueryResult } from "pg";
 import { Cars } from "../models/Cars";
 
 export interface ICars{
     create(owner: string, license_plate: string, telephone: string): Promise<void>;
-    findByID(id: string): Promise<void>;
-    verifyLicensePlate(license_plate: string): Promise<void>;
-    listAllCars(): Promise<QueryResult<Cars>[]>;
+    findByID(id: string): Promise<Cars>;
+    verifyLicensePlate(license_plate: string): Promise<Cars>;
+    listAllCars(): Promise<Cars[]>;
 }
