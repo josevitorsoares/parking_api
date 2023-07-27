1f69f7c497cda5d60566a01d8abc83e8cf15f7b3
josevitorsoares <josevitorgoncalvessoares@gmail.com>
2023-07-27 06:47:47 -0300

    FEAT: Adding listAvailableVacancies method

--- src/repositories/VacanciesRepository.ts
+++ src/repositories/VacanciesRepository.ts
@@ -4,7 +4,8 @@ export interface IVacancies{
     create(vacancy_number: string): Promise<void>;
     updateAvailableVacancy(vacancy_id: string, status: boolean): Promise<void>;
     findByID(id: string): Promise<Vacancies>;
-    verifyAvaliableVacancy(vacancy_id: string): Promise<Vacancies>;
+    verifyAvailableVacancy(vacancy_id: string): Promise<Vacancies>;
     verifyAllAvailableVacancies(): Promise<number>;
-    verifyVacancyNumber(vacancy_number: string): Promise<Vacancies>; 
+    verifyVacancyNumber(vacancy_number: string): Promise<Vacancies>;
+    listAvailableVacancies(): Promise<Vacancies[]>;
 }
