d2a40454a6e4004b8193302ff087a360d8c0ff64
josevitorsoares <josevitorgoncalvessoares@gmail.com>
2023-08-04 06:38:02 -0300

    REFACTIR: Adding checks moved from the use case

--- src/useCases/createVacancy.ts
+++ src/useCases/createVacancy.ts
@@ -1,13 +1,20 @@
-import { VacanciesRepository } from "../repositories/connection/ConnectionVacanciesRepository";
+import { AppError } from "../http/middlwares/AppError";
+import { IVacanciesRepository } from "../repositories/VacanciesRepository";
 
 interface CreateVacancyRequest {
     vacancy_number: string;
 }
 
 export class CreateVacancyUseCase {
-    constructor(private vacanciesRepository: VacanciesRepository){}
+    constructor(private vacanciesRepository: IVacanciesRepository){}
 
     async execute({ vacancy_number }: CreateVacancyRequest) {
+        const vacancyNumber = await this.vacanciesRepository.verifyVacancyNumber(vacancy_number);
+        
+        if (vacancyNumber) {
+            throw new AppError("Vacancy number already exists", 422);
+        }
+        
         await this.vacanciesRepository.create(vacancy_number);
     }
 }
