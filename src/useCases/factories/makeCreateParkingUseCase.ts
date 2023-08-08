2e61e4ddef670baea2580d6501abbec67390c5f3
josevitorsoares <josevitorgoncalvessoares@gmail.com>
2023-08-08 06:28:00 -0300

    REFACTIR: Adding new repositories

--- src/useCases/factories/makeCreateParkingUseCase.ts
+++ src/useCases/factories/makeCreateParkingUseCase.ts
@@ -1,9 +1,18 @@
+import { CarsRepository } from "../../repositories/connection/ConnectionCarsRepository";
 import { ParkingsRepository } from "../../repositories/connection/ConnectionParkingRepository";
+import { VacanciesRepository } from "../../repositories/connection/ConnectionVacanciesRepository";
 import { CreateParkingUseCase } from "../createParking";
 
 export function makeCreateParkgingUseCase() {
     const parkingsRepository = new ParkingsRepository();
-    const useCase = new CreateParkingUseCase(parkingsRepository);
+    const vacaciesRepository = new VacanciesRepository();
+    const carsRepository = new CarsRepository();
+    
+    const useCase = new CreateParkingUseCase(
+        parkingsRepository, 
+        vacaciesRepository, 
+        carsRepository
+    );
 
     return useCase;
 }
