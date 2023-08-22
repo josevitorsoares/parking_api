8402955de02ae78b9c6e8de4b9b3082e8da275e8
josevitorsoares <josevitorgoncalvessoares@gmail.com>
2023-08-22 06:45:13 -0300

    REFACTIR: Changing response format

--- src/http/controllers/parking/sumAmount.ts
+++ src/http/controllers/parking/sumAmount.ts
@@ -6,5 +6,5 @@ export async function sumAmount(request: Request, response: Response): Promise<R
 
     const result = await sumAmountUseCase.execute();
 
-    return response.json(result);
+    return response.json({message: !result ? `${result}` : "There was no completed parking today"});
 }
